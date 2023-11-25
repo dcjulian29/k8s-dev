30f162a834e52a0ae6dad1777b35f9b01d4ffd0a
Julian Easterling <julian@julianscorner.com>
2023-11-25 15:46:17 -0500

    Added kubectl alias command

---
+++ cmd/control.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2023 Julian Easterling <julian@julianscorner.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var controlCmd = &cobra.Command{
+	Use:                "control",
+	Short:              "Control the Kubernetes development vagrant environment",
+	Long:               "Control the Kubernetes development vagrant environment",
+	Aliases:            []string{"kubectl", "ctl"},
+	DisableFlagParsing: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		params := append(args, "--kubeconfig=./.kubectl.cfg")
+
+		output, _ := executeCommand("kubectl", params...)
+
+		fmt.Printf(output)
+	},
+	PreRun: func(cmd *cobra.Command, args []string) {
+		ensureRootDirectory()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(controlCmd)
+}
