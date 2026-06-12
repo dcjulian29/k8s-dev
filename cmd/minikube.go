b0fe266269a34dc81ff2f64097eea142babd852e
Julian Easterling <julian@julianscorner.com>
2026-06-12 19:02:00 -0400

    Updated copyright

--- cmd/minikube.go
+++ cmd/minikube.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 Julian Easterling <julian@julianscorner.com>
+Copyright © 2026 Julian Easterling
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
